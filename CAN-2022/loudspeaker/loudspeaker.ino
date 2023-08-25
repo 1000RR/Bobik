bb65026717b109b9a600f5ad4fff4f90e05eceb4
Anton Uzun <lysol17@gmail.com>
2023-08-25 13:11:36 -0700

    renames and moving things around

--- CAN-2022/loudspeaker/loudspeaker.ino
+++ CAN-2022/loudspeaker/loudspeaker.ino
@@ -38,23 +38,18 @@
 // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
 #define DREQ 3       // VS1053 Data request, ideally an Interrupt pin
 
-Adafruit_VS1053_FilePlayer musicPlayer = 
-  // create breakout-example object!
-  //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
-  // create shield-example object!
-  Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
+Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
   
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Adafruit VS1053 Simple Test");
+  Serial.begin(115200);
 
   if (! musicPlayer.begin()) { // initialise the music player
      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
      while (1);
   }
-  Serial.println(F("VS1053 found"));
+  Serial.println(F("VS1053 found {Serial}"));
   
-   if (!SD.begin(CARDCS)) {
+  if (!SD.begin(CARDCS)) {
     Serial.println(F("SD failed, or not present"));
     while (1);  // don't do anything more
   }
@@ -84,34 +79,36 @@ void setup() {
 }
 
 void loop() {
-  // File is playing in the background
-  if (musicPlayer.stopped()) {
-    Serial.println("Done playing music");
-    while (1) {
-      delay(10);  // we're done! do nothing...
-    }
-  }
-  if (Serial.available()) {
-    char c = Serial.read();
-    
-    // if we get an 's' on the serial console, stop!
-    if (c == 's') {
-      musicPlayer.stopPlaying();
-    }
-    
-    // if we get an 'p' on the serial console, pause/unpause!
-    if (c == 'p') {
-      if (! musicPlayer.paused()) {
-        Serial.println("Paused");
-        musicPlayer.pausePlaying(true);
-      } else { 
-        Serial.println("Resumed");
-        musicPlayer.pausePlaying(false);
-      }
-    }
-  }
+//  // File is playing in the background
+//  if (musicPlayer.stopped()) {
+//    Serial.println("Done playing music");
+//    while (1) {
+//      delay(10);  // we're done! do nothing...
+//    }
+//  }
+//  if (Serial.available()) {
+//    char c = Serial.read();
+//    
+//    // if we get an 's' on the serial console, stop!
+//    if (c == 's') {
+//      musicPlayer.stopPlaying();
+//    }
+//    
+//    // if we get an 'p' on the serial console, pause/unpause!
+//    if (c == 'p') {
+//      if (! musicPlayer.paused()) {
+//        Serial.println("Paused");
+//        musicPlayer.pausePlaying(true);
+//      } else { 
+//        Serial.println("Resumed");
+//        musicPlayer.pausePlaying(false);
+//      }
+//    }
+//  }
+//
+//  delay(100);
 
-  delay(100);
+
 }
 
 
