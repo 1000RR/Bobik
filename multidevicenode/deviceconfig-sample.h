03b477f6ff965e6f635e00d8f5de2e0d017c6428
Anton Uzun <lysol17@gmail.com>
2025-09-19 18:38:23 -0700

    deviceConfig files comments and updates

--- multidevicenode/deviceconfig-sample.h
+++ multidevicenode/deviceconfig-sample.h
@@ -6,7 +6,7 @@ Device devices[] = {
 		myCanId : 0x31, /*garage side door*/
 		deviceType : 5,
 		ioPin : 5,
-		relayPin : -1,	 /* -1 = no relay; writing LOW to this turns relay ON on a sensor */
+		relayPin : -1,	 /* -1 = no relay; writing LOW to this pin (when defined) turns relay ON a sensor */
 		sensorVal : LOW, /* variable to store the sensor status (value) */
 		isAlarmed : false,
 		nextStateChangeTimestamp : 0,
@@ -15,10 +15,10 @@ Device devices[] = {
 	},
 	{
 		type : SENSOR,
-		myCanId : 0x50, /*front of house*/
+		myCanId : 0x50, /*front of house door*/
 		deviceType : 5,
 		ioPin : 6,
-		relayPin : -1,	 /* -1 = no relay; writing LOW to this turns relay ON on a sensor */
+		relayPin : -1,	 /* -1 = no relay; writing LOW to this pin (when defined) turns relay ON a sensor */
 		sensorVal : LOW, /* variable to store the sensor status (value) */
 		isAlarmed : false,
 		nextStateChangeTimestamp : 0,
@@ -27,13 +27,15 @@ Device devices[] = {
 	},
 	{
 		type : SENSOR,
-		myCanId : 0x40, /*back door kitchen*/
+		myCanId : 0x40, /*back kitchen door*/
 		deviceType : 5,
 		ioPin : 4,
-		relayPin : -1,	 /* -1 = no relay; writing LOW to this turns relay ON on a sensor */
+		relayPin : -1,	 /* -1 = no relay; writing LOW to this pin (when defined) turns relay ON a sensor */
 		sensorVal : LOW, /* variable to store the sensor status (value) */
 		isAlarmed : false,
 		nextStateChangeTimestamp : 0,
 		isEnabled : true, /* false = off; true = on; has direct effect on relay state if relay present */
 		buzzerDirection : false
 	}};
+
+// type: SENSOR and relayPin > -1 means a powered sensor device that can be enabled/disabled
