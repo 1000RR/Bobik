223d50b5ab2172b34389a617341fc4ac07847ce7
Anton Uzun <lysol17@gmail.com>
2025-09-22 18:06:53 -0700

    bugfix: SENSOR type: only react to valid messages

--- multidevicenode/multidevicenode.ino
+++ multidevicenode/multidevicenode.ino
@@ -284,10 +284,11 @@ void setStateAccordingToMessage(int deviceNum) {
 	if (devices[deviceNum].type == SENSOR) {
 		if (incomingCanMsg.data[1] == COMMAND_ENABLE_DEVICE) {	// enable
 			newState = true;
+			setSensorEnableState(deviceNum, newState);
 		} else if (incomingCanMsg.data[1] == COMMAND_DISABLE_DEVICE) {	// disable
 			newState = false;
+			setSensorEnableState(deviceNum, newState);
 		}
-		setSensorEnableState(deviceNum, newState);
 	} else if (devices[deviceNum].type == ALARM) {
 		if (incomingCanMsg.data[1] == COMMAND_TURN_ON_ALARM) {
 			devices[deviceNum].isAlarmed = true;
@@ -317,7 +318,7 @@ void loop() /* go through all devices: read incoming CAN message, send message f
 		if (devices[i].type == SENSOR) {
 			devices[i].sensorVal = digitalRead(devices[i].ioPin); /*LOW by default, no motion detected*/
 			if (debugSerialOutput) {
-				Serial.println(String("Read SENSOR 0x") + String(devices[i].myCanId, HEX) + String (" at pin ") + String(devices[i].ioPin) + String(" with value: ") + String(devices[i].sensorVal));
+				Serial.println(String("Read SENSOR 0x") + String(devices[i].myCanId, HEX) + String(" at pin ") + String(devices[i].ioPin) + String(" with value: ") + String(devices[i].sensorVal));
 			}
 			if (enableLocalBuzzer) {
 				localBuzzerSounding = localBuzzerSounding || (devices[i].sensorVal == HIGH && devices[i].isEnabled == true);
