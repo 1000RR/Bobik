52a702ac725f97562ded39c737f9fb5a7cedcc0a
Anton Uzun <lysol17@gmail.com>
2025-09-25 13:38:13 -0700

    restyle special functions buttons

--- frontend/app/components/SpecialFunctions.tsx
+++ frontend/app/components/SpecialFunctions.tsx
@@ -28,7 +28,7 @@ const PanelColorStyle = css`
     background-color: rgba(60,60,60, .3);
 `;
 
-const CompositePanelStyle = styled.div`
+const RowCompositePanelStyle = styled.div`
     ${PanelSizeStyle}
     ${PanelLayoutStyle}
     ${PanelColorStyle}
@@ -74,32 +74,36 @@ const SpecialFunctions: React.FC<{
     const dispatch = useDispatch();
 
     return (
-        <CompositePanelStyle className={className}>
-            <Button className="blueButton dimmable" onClick={clearDataHandler}>Clear Data</Button>
-            <Button className="orangeButton dimmable" onClick={testAlarmsHandler}>Test Alarms (current profile-selected)</Button>
-            <Button className="redButton dimmable" onClick={getAttentionHandler}>Get Attention</Button>
-            <Panel>
-            <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
-                <input type="text" id="sender-field" className="dimmable input-field" name="sender" defaultValue="0x75" required />
-                <span className="input-hint" style={{position: "absolute", right: 7}}>FROM</span>
-            </div>
-            <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
-                <input type="text" id="receiver-field" className="dimmable input-field" name="receiver" defaultValue="0x14" required />
-                <span className="input-hint" style={{position: "absolute", right: 7}}>TO</span>
-            </div>
-            <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
-                <input type="text" id="message-field" className="dimmable input-field" name="message" defaultValue="0xAA" required />
-                <span className="input-hint" style={{position: "absolute", right: 7}}>MSG</span>
-            </div>
-            <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
-                <input type="text" id="type-field" className="dimmable input-field" name="type" defaultValue="0x00" required />
-                <span className="input-hint" style={{position: "absolute", right: 7}}>TYPE</span>
-            </div>  
-            <Button id="can-send-single" className="smallbutton gray dimmable" onClick={sendSingleCan}>send once</Button>
-            <Button id="can-send-repeatedly" className="smallbutton gray dimmable" onClick={sendRepeatedlyCan}>send repeatedly</Button>
-            <Button id="can-stop-send" className="smallbutton gray dimmable" onClick={stopSendingCan}>stop sending</Button>
-            </Panel>
-        </CompositePanelStyle>
+        <>
+        <RowCompositePanelStyle className={className}>
+                <Button className="mediumbutton blueButton dimmable" onClick={clearDataHandler}>Clear Data</Button>
+                <Button className="mediumbutton orangeButton dimmable" onClick={testAlarmsHandler}>Test Alarms (current profile-selected)</Button>
+                <Button className="mediumbutton redButton dimmable" onClick={getAttentionHandler}>Get Attention</Button>
+        </RowCompositePanelStyle>
+        <Panel>
+        <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
+            <input type="text" id="sender-field" className="dimmable input-field" name="sender" defaultValue="0x75" required />
+            <span className="input-hint" style={{position: "absolute", right: 7}}>FROM</span>
+        </div>
+        <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
+            <input type="text" id="receiver-field" className="dimmable input-field" name="receiver" defaultValue="0x14" required />
+            <span className="input-hint" style={{position: "absolute", right: 7}}>TO</span>
+        </div>
+        <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
+            <input type="text" id="message-field" className="dimmable input-field" name="message" defaultValue="0xAA" required />
+            <span className="input-hint" style={{position: "absolute", right: 7}}>MSG</span>
+        </div>
+        <div className="input-wrapper" style={{position: "relative", display: "inline-block"}}>
+            <input type="text" id="type-field" className="dimmable input-field" name="type" defaultValue="0x00" required />
+            <span className="input-hint" style={{position: "absolute", right: 7}}>TYPE</span>
+        </div>
+        </Panel>
+        <RowCompositePanelStyle className={className}>
+            <Button id="can-send-single" className="smallbutton gray dimmable" onClick={sendSingleCan}>send 1x</Button>
+            <Button id="can-send-repeatedly" className="smallbutton gray dimmable" onClick={sendRepeatedlyCan}>send Nx</Button>
+            <Button id="can-stop-send" className="smallbutton gray dimmable" onClick={stopSendingCan}>stop</Button>
+        </RowCompositePanelStyle>
+        </>
     );
 };
 
