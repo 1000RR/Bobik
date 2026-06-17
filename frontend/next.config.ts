16eac1eee8218d175e6efafaebe94548ab4c13e5
Anton Uzun <lysol17@gmail.com>
2026-06-16 18:24:53 -0700

    canbus-sniffer moved out of project; passwordchecker moved into frontend/public; package.json outputs build IDs to STATIC_BUILD_IDS in root dir - gitignored

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,37 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob:;
+  font-src 'self' data:;
+  connect-src 'self';
+  frame-ancestors 'none';
+  base-uri 'self';
+  form-action 'self';
+  object-src 'none';
+  upgrade-insecure-requests;
+`.replace(/\s{2,}/g, " ").trim();
+
+const SharedConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy,
+          },
+        ],
+      },
+    ];
+  }
+};
+
 const staticConfig: NextConfig = {
+  ...SharedConfig,
   output: "export",
   images: {
     unoptimized: true,
@@ -8,13 +39,7 @@ const staticConfig: NextConfig = {
 };
 
 const ssrConfig: NextConfig = {
-  async generateBuildId() {
-    // Use a version from env or fallback to timestamp
-    return `build-${process.env.NEXT_PUBLIC_ASSET_VERSION || Date.now()}`;
-  }
+  ...SharedConfig
 };
-
-
-
 
 export default (process.env.NEXT_BUILD_CONFIG === 'static' ? staticConfig : ssrConfig);
